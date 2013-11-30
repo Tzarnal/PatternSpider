51f9b5f2e4dfe2ab90146c48bae6ef0b1a36a72c
Xesyto <M8R-acpfdp@mailinator.com>
2013-11-30 18:22:36 +0100

    Added a Replace plugin that allows you to correct what has been said before in the classical s/x/y syntax. Keeps a multiline personal as well as general history to match too.

---
+++ Plugin-Replace/LineHistory.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using System.Management.Instrumentation;
+
+namespace Plugin_Replace
+{
+    class LineHistory
+    {
+        private string[] _history;
+
+        public LineHistory(int size)
+        {
+            _history = new string[size];
+            for (var i = 0; i < _history.Length ; i++)
+            {
+                _history[i] = string.Empty;
+            }
+        }
+
+        public bool HasMatch(string text)
+        {
+            return _history.Any(line => line.Contains(text));
+        }
+
+        public void AddLine(string line)
+        {
+            //move all entries in _history down one
+            for (var i = 0; i < _history.Length -1; i++)
+            {
+                _history[i] = _history[i+1];
+            }
+
+            //add new line
+            _history[_history.Length-1] = line;
+        }
+
+        public string Replace(string original, string replacement)
+        {
+            foreach (var line in _history)
+            {
+                if (line.Contains(original))
+                {
+                    return line.Replace(original, replacement);
+                }
+            }
+
+            throw new InstanceNotFoundException("Could not find "+original+" in History");
+        }
+    }
+}
---
+++ Plugin-Replace/Replace.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Text.RegularExpressions;
+using IrcDotNet;
+using PatternSpider.Irc;
+using PatternSpider.Plugins;
+
+namespace Plugin_Replace
+{
+    [Export(typeof(IPlugin))]
+    class Replace : IPlugin
+    {
+        public string Name { get { return "Replace"; } }
+        public string Description { get { return "Uses the s/bla/bloop/ notation to correct line spreviously said"; } }
+
+        public List<string> Commands { get { return new List<string>(); } }
+
+        private Dictionary<string, LineHistory> _history;
+        private LineHistory _generalHistory;
+
+        public Replace()
+        {
+            _history = new Dictionary<string, LineHistory>();
+            _generalHistory = new LineHistory(10);
+        }
+
+        public List<string> IrcCommand(IrcBot ircBot, string server, IrcMessageEventArgs e)
+        {
+            return null;
+        }
+
+        public List<string> OnChannelMessage(IrcBot ircBot, string server, string channel, IrcMessageEventArgs e)
+        {
+            var id = channel + e.Source.Name;
+            var line = e.Text;
+            var expresion = @"[sr][/\\\\](.+)[/\\\\](.+)";
+
+            if (!_history.ContainsKey(id))
+            {
+                _history.Add(id, new LineHistory(3));
+            }
+
+            if (Regex.IsMatch(line, expresion))
+            {
+                var match = Regex.Match(line, expresion);
+                var original = match.Groups[1].Value;
+                var replacement = match.Groups[2].Value;
+                
+
+                var rLastChar = replacement.Length-1;
+                if(replacement[rLastChar] == '\\' || replacement[rLastChar] == '/')
+                {
+                    replacement = replacement.Substring(0, rLastChar);
+                }
+
+
+                if (_history[id].HasMatch(original))
+                {
+                    return new List<string>{_history[id].Replace(original,replacement)};                    
+                }
+                
+                if (_generalHistory.HasMatch(original))
+                {
+                    return new List<string> {_generalHistory.Replace(original, replacement)};
+                }
+
+                return null;
+            }
+            
+
+            _generalHistory.AddLine(line);
+            _history[id].AddLine(line);
+
+            return null;
+        }
+
+        public List<string> OnUserMessage(IrcBot ircBot, string server, IrcMessageEventArgs e)
+        {
+            return null;
+        }
+    }
+}
