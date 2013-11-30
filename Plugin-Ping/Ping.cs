10a1b996b75954e4cd3a346e4470634b803ed981
Xesyto <M8R-acpfdp@mailinator.com>
2013-11-30 15:06:26 +0100

    Added a simply Ping plugin.

---
+++ Plugin-Ping/Ping.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using IrcDotNet;
+using PatternSpider.Irc;
+using PatternSpider.Plugins;
+
+namespace Plugin_Ping
+{
+    [Export(typeof(IPlugin))]
+    class Ping : IPlugin
+    {
+        public string Name { get { return "Ping"; } }
+        public string Description { get { return "Returns a pong, when invoked with !ping"; } }
+
+        public List<string> Commands { get { return new List<string> { "ping" }; } }
+
+        
+        public List<string> IrcCommand(IrcBot ircBot, string server, IrcMessageEventArgs e)
+        {
+            return new List<string>{"Pong."};
+        }
+
+        public List<string> OnChannelMessage(IrcBot ircBot, string server, string channel, IrcMessageEventArgs e)
+        {
+            return null;
+        }
+
+        public List<string> OnUserMessage(IrcBot ircBot, string server, IrcMessageEventArgs e)
+        {
+            return null;
+        }
+    }
+}
