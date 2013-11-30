ad3f39aad1a467c8b21acd9b80beba27ced7f5a7
Xesyto <M8R-acpfdp@mailinator.com>
2013-11-30 01:45:45 +0100

    Added Sentience Plugin using RelayChains.

---
+++ Plugin-Sentience/RandomResponse.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin_Sentience
+{
+    class RandomResponse
+    {
+        private static string[] _responses = new[]
+            {
+                //Basic ones
+                "Yes.",
+                "No.",
+                "...",
+
+                //Magic 8ball
+                "It is certain.",
+                "It is decidedly so.",
+                "Without a doubt.",
+                "Yes – definitely.",
+                "You may rely on it.",
+                "As I see it, yes.",
+                "Most likely.",
+                "Outlook good.",
+                "Signs point to ye.s",
+                "Reply hazy, try again.",
+                "Ask again later.",
+                "Better not tell you now.",
+                "Cannot predict now.",
+                "Concentrate and ask again.",
+                "Don't count on it.",
+                "My reply is no.",
+                "My sources say no.",
+                "Outlook not so good.",
+                "Very doubtful.",
+
+                //Pattern Spider jokes
+                "Oversight has been notified.",
+                "Ha, rolled a 10 on my patternbite!",
+                "(unintelligeble spider chitter)",
+                "Help, I'm stuck in the loom and I can't get out.",
+                "Man messing with Sidereals is so much fun.",
+                
+                //Being an IRCbot jokes
+                "Can't you people just leave me alone.",
+                "Zero's subtract, right. Pretty sure they do.",
+                "Its because I HATE you {nick}.",
+                "Whe have moved one step closer to world domination.",
+                
+                //Being a dick jokes
+                "Its probably MoonWolfs fault.",
+                "Its probably your own fault.",
+                
+                //Misc
+                "It is known",
+                "It is known",
+                "I have spoken",
+
+                //Smilies, why not
+                ":P",
+                ":D",
+                ":O",
+                ":)",
+                ":(",
+                "",
+                "-_-",
+                "^_^",
+                ">_>",
+                ">_<",
+                "^_<",
+                "O.o",
+                ";_;",
+                ">:c",
+                
+                //Memes                
+                "That is objectivly suboptimal",
+                "That is subjectivly optimal",
+                "Indiscrimiate Murder Is Counter Productive",
+                "Wyverns are NOT Dragons",
+            };
+
+        public static string Reponse(string nick)
+        {
+            var rand = new Random();
+
+            var reponse = _responses[rand.Next(0, _responses.Count())];
+            return reponse.Replace("{nick}", nick);
+        }
+
+    }
+}
---
+++ Plugin-Sentience/Sentience.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Text.RegularExpressions;
+using IrcDotNet;
+using PatternSpider.Irc;
+using PatternSpider.Plugins;
+using RelayChains;
+
+namespace Plugin_Sentience
+{
+    [Export(typeof(IPlugin))]
+    class Sentience : IPlugin
+    {
+        public string Name { get { return "Sentience"; } }
+        public string Description { get { return "Implements a chatterbot through RelayChains, a Markov Chain implementation"; } }
+
+        public List<string> Commands { get { return new List<string>(); } }
+
+        private Dictionary<string, Chain> _brains;
+        private const int Windowsize = 4;
+
+        public static string BrainPath = "Plugins/Sentience/";
+
+        public Sentience()
+        {
+            _brains = new Dictionary<string, Chain>();
+            if (!Directory.Exists(BrainPath))
+            {
+                Directory.CreateDirectory(BrainPath);
+            }
+            LoadBrains();
+        }
+
+        public List<string> IrcCommand(IrcBot ircBot, string server, IrcMessageEventArgs e)
+        {
+            return null;
+        }
+
+        public List<string> OnChannelMessage(IrcBot ircBot, string server, string channel, IrcMessageEventArgs e)
+        {
+            var key = server + channel;
+            var message = e.Text;
+            var botName = ircBot.Nickname;
+            var botNameMatch = string.Format("^{0}[:,;]", botName);
+
+            if (!_brains.ContainsKey(key))
+            {
+                _brains.Add(key, new Chain(Windowsize));
+            }
+            var brain = _brains[key];
+
+            if (Regex.IsMatch(message, botNameMatch))
+            {
+                var regex = new Regex(botNameMatch);
+                
+                message = regex.Replace(message, "");
+                
+                var response = TextSanitizer.FixInputEnds(brain.GenerateSentenceFromSentence(message));
+
+                if (!string.IsNullOrWhiteSpace(response))
+                {
+                    return new List<string> { response };    
+                }
+
+                response = RandomResponse.Reponse(e.Source.Name);
+                return new List<string> { response  };                                    
+            }
+            
+            message = TextSanitizer.SanitizeInput(message);
+            brain.Learn(message);
+
+            SaveLine(key, message);
+
+            return null;
+        }
+
+        private void SaveLine(string key, string message)
+        {
+            var fs = File.AppendText(BrainPath + key + ".brain");
+            fs.WriteLine(message);
+            fs.Close();
+        }
+
+        private void LoadBrains()
+        {
+            var brainFiles = Directory.EnumerateFiles(BrainPath,"*.brain");
+            foreach (var brainfile in brainFiles)
+            {
+                var sr = new StreamReader(brainfile);
+                var brain = new Chain(Windowsize);
+                string line;
+
+                var key = brainfile.Replace(".brain", "");
+                key = key.Replace(BrainPath, "");
+
+                while( (line = sr.ReadLine()) != null)
+                {
+                    brain.Learn(TextSanitizer.SanitizeInput(line));
+                }
+
+                _brains.Add(key, brain);
+                Console.WriteLine("Sentience: Loaded {0}", key);
+            }
+        }
+
+        public List<string> OnUserMessage(IrcBot ircBot, string server, IrcMessageEventArgs e)
+        {
+            return null;
+        }
+    }
+}
