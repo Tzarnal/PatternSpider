3bfffd4111bf3f575c78be8480ba1ee681b0deef
Xesyto <M8R-acpfdp@mailinator.com>
2014-11-04 19:27:01 +0100

    Added Project for MTG Card searching Plugin.

---
+++ Plugin-MTG/MTG.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using IrcDotNet;
+using PatternSpider.Irc;
+using PatternSpider.Plugins;
+
+namespace Plugin_MTG
+{
+    [Export(typeof(IPlugin))]
+    public class MTG : IPlugin
+    {
+        public string Name { get { return "MTG"; } }
+        public string Description { get { return "Gives a link to a MTG card when invoked."; } }
+
+        public List<string> Commands { get { return new List<string> { "mtg" }; } }
+        
+        public List<string> IrcCommand(IrcBot ircBot, string server, IrcMessageEventArgs e)
+        {
+            var text = e.Text.Trim();
+            var messageParts = text.Split(' ');
+            var searchString = string.Join(" ", messageParts.Skip(1));
+
+            
+
+            return new List<string> { "Card" };            
+        }
+
+        public List<string> OnChannelMessage(IrcBot ircBot, string server, string channel, IrcMessageEventArgs e)
+        {
+            return null;
+        }
+
+        public List<string> OnUserMessage(IrcBot ircBot, string server, IrcMessageEventArgs e)
+        {
+            return null;        
+        }
+
+        private HtmlDocument UrlRequest(string url)
+        {
+            var req = (HttpWebRequest)WebRequest.Create(url);
+            req.Method = "GET";
+            req.ContentType = "application/x-www-form-urlencoded";
+            req.Headers.Add("Accept-Language", "en;q=0.8");
+            req.UserAgent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2";
+
+            var responseStream = req.GetResponse().GetResponseStream();
+            var document = new HtmlDocument();
+
+            if (responseStream == null)
+            {
+                throw new NoNullAllowedException();
+            }
+
+            using (var reader = new StreamReader(responseStream))
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    using (var writer = new StreamWriter(memoryStream))
+                    {
+                        writer.Write(reader.ReadToEnd());
+                        memoryStream.Position = 0;
+                        document.Load(memoryStream, new UTF8Encoding());
+                    }
+                }
+            }
+
+            return document;
+        }
+    }
+}
