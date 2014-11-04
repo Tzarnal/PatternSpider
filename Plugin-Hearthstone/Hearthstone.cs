87518767b05cf12afb4670fa35c71919e7787abf
Xesyto <M8R-acpfdp@mailinator.com>
2014-11-04 12:20:22 +0100

    Added project for a Hearthstone Card plugin.

---
+++ Plugin-Hearthstone/Hearthstone.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using IrcDotNet;
+using PatternSpider.Irc;
+using PatternSpider.Plugins;
+
+namespace Plugin_Hearthstone
+{
+    [Export(typeof(IPlugin))]
+    public class Hearthstone : IPlugin
+    {
+        public string Name { get { return "Hearthstone"; } }
+        public string Description { get { return "Gives a link to a hearhstone card when invoked."; } }
+
+        public List<string> Commands { get { return new List<string> { "hs" }; } }
+
+
+        public List<string> IrcCommand(IrcBot ircBot, string server, IrcMessageEventArgs e)
+        {
+            return new List<string> { "Card." };
+        }
+
+        public List<string> OnChannelMessage(IrcBot ircBot, string server, string channel, IrcMessageEventArgs e)
+        {
+            return null;
+        }
+
+        public List<string> OnUserMessage(IrcBot ircBot, string server, IrcMessageEventArgs e)
+        {
+            return null;
+        }
+    }
+}
