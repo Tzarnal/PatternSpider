d4afab3aba2c43eb28d5aec2eca6f50b7c412a04
Xesyto <M8R-acpfdp@mailinator.com>
2014-11-04 15:41:30 +0100

    Implemented HS search plugin functionality.

---
+++ Plugin-Hearthstone/Card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Plugin_Hearthstone
+{
+    public class Card
+    {
+        public int id { get; set; }
+        public string image { get; set; }
+        public int set { get; set; }
+        public int quality { get; set; }
+        public string icon { get; set; }
+        public int type { get; set; }
+        public int cost { get; set; }
+        public int attack { get; set; }
+        public int health { get; set; }
+        public int faction { get; set; }
+        public int elite { get; set; }
+        public int collectible { get; set; }
+        public int race { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public int popularity { get; set; }
+    }
+}
--- Plugin-Hearthstone/Hearthstone.cs
+++ Plugin-Hearthstone/Hearthstone.cs
@@ -1,8 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
 using IrcDotNet;
 using PatternSpider.Irc;
 using PatternSpider.Plugins;
+using Newtonsoft.Json;
 
 namespace Plugin_Hearthstone
 {
@@ -17,7 +26,13 @@ namespace Plugin_Hearthstone
 
         public List<string> IrcCommand(IrcBot ircBot, string server, IrcMessageEventArgs e)
         {
-            return new List<string> { "Card." };
+            var text = e.Text.Trim();
+            var messageParts = text.Split(' ');
+            var searchString = string.Join(" ",messageParts.Skip(1));
+
+            var searchResult = SearchHearthHead(searchString);
+
+            return new List<string> { searchResult };
         }
 
         public List<string> OnChannelMessage(IrcBot ircBot, string server, string channel, IrcMessageEventArgs e)
@@ -28,6 +43,89 @@ namespace Plugin_Hearthstone
         public List<string> OnUserMessage(IrcBot ircBot, string server, IrcMessageEventArgs e)
         {
             return null;
+        }
+
+        private string SearchHearthHead(string searchString)
+        {
+            HtmlDocument document;
+            
+            try
+            {
+                document = UrlRequest(string.Format("http://www.hearthhead.com/cards=?filter=na={0}'", searchString));
+            }
+            catch
+            {
+                return "Error Occured trying to search for card.";
+            }
+
+            var jsonString = ExtractJsonStringFromPage(document);
+            
+            if (jsonString == null)
+                return "No Results found for: " + searchString;
+
+            var cards = PraseJson(jsonString);
+
+            if (cards.Count == 1)
+            {
+                var card = cards[0];
+                return String.Format("[http://www.hearthhead.com/card={5}] {0} - {1}/{2} for {3} mana - {4}", card.name, card.attack, card.health, card.cost, card.description, card.id);
+            }
+
+
+            return String.Format("[http://www.hearthhead.com/cards=?filter=na={1}] Found {0} cards.", cards.Count, searchString);
+        }
+
+        private List<Card> PraseJson(string data)
+        {
+            return JsonConvert.DeserializeObject<List<Card>>(data);
+        }
+        
+        private string ExtractJsonStringFromPage(HtmlDocument document)
+        {
+            string flatDocument = document.DocumentNode.InnerHtml;
+            
+            var matchString = @"hearthstoneCards.=.(\[([^]]+)\]);";
+            var regex = new Regex(matchString, RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
+
+            if (regex.IsMatch(flatDocument))
+            {
+                var match = regex.Match(flatDocument);
+                return match.Groups[1].Value;
+            }
+
+            return null;
+        }
+
+        private HtmlDocument UrlRequest(string url)
+        {
+            var req = (HttpWebRequest)WebRequest.Create(url);
+            req.Method = "GET";
+            req.ContentType = "application/x-www-form-urlencoded";
+            req.Headers.Add("Accept-Language", "en;q=0.8");
+            req.UserAgent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2";
+
+            var responseStream = req.GetResponse().GetResponseStream();
+            var document = new HtmlDocument();
+
+            if (responseStream == null)
+            {
+                throw new NoNullAllowedException();
+            }
+
+            using (var reader = new StreamReader(responseStream))
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    using (var writer = new StreamWriter(memoryStream))
+                    {
+                        writer.Write(reader.ReadToEnd());
+                        memoryStream.Position = 0;
+                        document.Load(memoryStream, new UTF8Encoding());
+                    }
+                }
+            }
+
+            return document;
         }
     }
 }
