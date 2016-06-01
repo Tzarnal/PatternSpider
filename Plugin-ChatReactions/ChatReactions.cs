31113c133ef82a9fdb82fbf23250e778a4adc19c
Xesyto <s.oudenaarden@gmail.com>
2016-06-01 12:28:51 +0200

    Added a Chat Reactions plugin. Implemented a years old meme about tables. (╯°□°)╯︵ ┻━┻

---
+++ Plugin-ChatReactions/ChatReactions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using PatternSpider.Irc;
+using PatternSpider.Plugins;
+
+namespace Plugin_ChatReactions
+{
+    [Export(typeof(IPlugin))]
+    public class ChatReactions : IPlugin
+    {
+        public string Name => "ChatReactions";
+        public string Description => "Automated reactions to certain things being said.";
+        public List<string> Commands => new List<string>();
+
+        public List<string> IrcCommand(IrcBot ircBot, string server, IrcMessage m)
+        {
+            return null;
+        }
+
+        public List<string> OnChannelMessage(IrcBot ircBot, string server, string channel, IrcMessage m)
+        {
+            if(m.Text.Contains("(╯°□°)╯︵ ┻━┻"))
+                return new List<string> { "┬──┬◡ﾉ(° -°ﾉ)" };
+
+            return null;
+        }
+                        
+        public List<string> OnUserMessage(IrcBot ircBot, string server, IrcMessage m)
+        {
+            return null;
+        }
+    }
+}
